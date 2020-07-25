95a8247be3235cb37e0fa00741615bc989c57150
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-25 21:22:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -45,8 +45,8 @@ class Person
         return "all about the benjamins"
     end
     def take_bath
-      binding.pry
-        @hygiene+=4
+    
+        self.hygiene+=4
         return "♪ Rub-a-dub just relaxing in the tub ♫"
 
     end
