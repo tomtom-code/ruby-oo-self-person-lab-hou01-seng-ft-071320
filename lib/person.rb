9e3bf222a8d78d5111d9cdcf30afdf667ee1ae21
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-25 21:40:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -70,8 +70,8 @@ class Person
     def start_conversation(person, topic)
 
       if topic == politics
-        self.happiness -= 1
-        friend.happiness -= 1
+        self.happiness -= 2
+        friend.happiness -= 2
       return "blah blah partisan blah lobbyist"
       elsif topic == weather
         self.happiness += 1
